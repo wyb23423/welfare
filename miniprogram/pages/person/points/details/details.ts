bb71dfc4c987777526be2e0a05ed96c2abeabd63
zengyuhan503 <13458685625@163.com>
2019-08-06 17:07:07 +0800

    积分明细

---
+++ miniprogram/pages/person/points/details/details.ts
@@ -0,0 +1,43 @@
+import { formatTime } from "../../../../utils/util";
+
+/**
+ * 积分明细
+ */
+interface DetailsItem {
+    title: string;
+    time: string;
+    value: number;
+}
+
+
+Page({
+    data: {
+        list: <DetailsItem[]>[]
+    },
+    onLoad() {
+        const now = Date.now();
+        const list: DetailsItem[] = [
+            {
+                title: '兑换海贼周边',
+                time: formatTime(new Date(now + this._getTestData())),
+                value: -100
+            },
+            {
+                title: '不良记录扣除',
+                time: formatTime(new Date(now + this._getTestData())),
+                value: -100
+            }
+        ];
+        for (let i = 0; i < 3; i++) {
+            list.push({
+                title: '公益活动参加',
+                time: formatTime(new Date(now + this._getTestData())),
+                value: [100, 500, 600][i]
+            })
+        }
+        this.setData!({ list });
+    },
+    _getTestData() {
+        return Math.round(Math.random() * 518400000) - 259200000;
+    }
+})
