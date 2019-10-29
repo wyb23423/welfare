28a5328e2fa84a7a19aad76ab07219a720721739
wyb <1354162430@163.com>
2019-10-29 16:20:45 +0800

    显示userid

--- miniprogram/components/person_head/person_head.js
+++ miniprogram/components/person_head/person_head.js
@@ -1,10 +1,17 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var store_1 = require("../../constant/store");
 Component({
     properties: {
         isindex: {
             type: Boolean,
             value: false
         }
+    },
+    ready: function () {
+        this.setData({
+            username: wx.getStorageSync(store_1.USER_NAME)
+        });
     }
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGVyc29uX2hlYWQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJwZXJzb25faGVhZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBR0EsU0FBUyxDQUFDO0lBQ04sVUFBVSxFQUFFO1FBQ1IsT0FBTyxFQUFFO1lBQ0wsSUFBSSxFQUFFLE9BQU87WUFDYixLQUFLLEVBQUUsS0FBSztTQUNmO0tBQ0o7Q0FDSixDQUFDLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKipcclxuICog5Liq5Lq65L+h5oGv6aG16aG26YOo5aS05YOP5Y+K5ZCN56ewXHJcbiAqL1xyXG5Db21wb25lbnQoe1xyXG4gICAgcHJvcGVydGllczoge1xyXG4gICAgICAgIGlzaW5kZXg6IHtcclxuICAgICAgICAgICAgdHlwZTogQm9vbGVhbixcclxuICAgICAgICAgICAgdmFsdWU6IGZhbHNlXHJcbiAgICAgICAgfVxyXG4gICAgfVxyXG59KTtcclxuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGVyc29uX2hlYWQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJwZXJzb25faGVhZC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUdBLDhDQUFpRDtBQUNqRCxTQUFTLENBQUM7SUFDTixVQUFVLEVBQUU7UUFDUixPQUFPLEVBQUU7WUFDTCxJQUFJLEVBQUUsT0FBTztZQUNiLEtBQUssRUFBRSxLQUFLO1NBQ2Y7S0FDSjtJQUNELEtBQUs7UUFDRCxJQUFJLENBQUMsT0FBTyxDQUFDO1lBQ1QsUUFBUSxFQUFFLEVBQUUsQ0FBQyxjQUFjLENBQUMsaUJBQVMsQ0FBQztTQUN6QyxDQUFDLENBQUM7SUFDUCxDQUFDO0NBQ0osQ0FBQyxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXHJcbiAqIOS4quS6uuS/oeaBr+mhtemhtumDqOWktOWDj+WPiuWQjeensFxyXG4gKi9cclxuaW1wb3J0IHsgVVNFUl9OQU1FIH0gZnJvbSAnLi4vLi4vY29uc3RhbnQvc3RvcmUnO1xyXG5Db21wb25lbnQoe1xyXG4gICAgcHJvcGVydGllczoge1xyXG4gICAgICAgIGlzaW5kZXg6IHtcclxuICAgICAgICAgICAgdHlwZTogQm9vbGVhbixcclxuICAgICAgICAgICAgdmFsdWU6IGZhbHNlXHJcbiAgICAgICAgfVxyXG4gICAgfSxcclxuICAgIHJlYWR5KCkge1xyXG4gICAgICAgIHRoaXMuc2V0RGF0YSh7XHJcbiAgICAgICAgICAgIHVzZXJuYW1lOiB3eC5nZXRTdG9yYWdlU3luYyhVU0VSX05BTUUpXHJcbiAgICAgICAgfSk7XHJcbiAgICB9XHJcbn0pO1xyXG4iXX0=
--- miniprogram/components/person_head/person_head.ts
+++ miniprogram/components/person_head/person_head.ts
@@ -1,11 +1,17 @@
 /**
  * 个人信息页顶部头像及名称
  */
+import { USER_NAME } from '../../constant/store';
 Component({
     properties: {
         isindex: {
             type: Boolean,
             value: false
         }
+    },
+    ready() {
+        this.setData({
+            username: wx.getStorageSync(USER_NAME)
+        });
     }
 });
