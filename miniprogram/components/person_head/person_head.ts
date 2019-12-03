665a983a37a70b68b69090f584778cfb6d877acc
wyb <1354162430@163.com>
2019-12-03 16:15:11 +0800

    更改社区认证按钮的文职

--- miniprogram/components/person_head/person_head.ts
+++ miniprogram/components/person_head/person_head.ts
@@ -5,7 +5,11 @@ import { USER_NAME, USER_AUTHENTICATION } from '../../constant/store';
 import { Authentication } from '../../constant/index';
 import { request } from '../../utils/http';
 
-Component({
+interface PersonHead extends WxComponent {
+    authentication(): void;
+}
+
+Component<PersonHead>({
     properties: {
         isindex: {
             type: Boolean,
@@ -16,14 +20,30 @@ Component({
             value: true
         }
     },
+    data: {
+        time: 0, // 上次点击头像时间
+        count: 0 // 连续点击头像次数
+    },
     ready() {
         const auth = wx.getStorageSync(USER_AUTHENTICATION);
-        this.setData({
-            username: wx.getStorageSync(USER_NAME),
-            hasCommodity: !(auth === Authentication.commodity || auth === Authentication.auditing)
-        });
+        this.data.hasCommodity = !(auth === Authentication.commodity || auth === Authentication.auditing);
+        this.setData({username: wx.getStorageSync(USER_NAME)});
     },
     methods: {
+        clickAvator() {
+            // tslint:disable-next-line:prefer-const
+            let {count, time, hasCommodity} = this.data;
+            if(!hasCommodity) {
+                return;
+            }
+            const now = Date.now();
+            if(now - time >= 500) {
+                count = 0;
+            }
+            this.data.time = now;
+            this.data.count = ++count;
+            count >= 5 && this.authentication();
+        },
         authentication() {
             wx.showModal({
                 content: '申请成为社区管理员?',
@@ -34,7 +54,8 @@ Component({
                             .then(() => this.setData({hasCommodity: false}))
                             .catch(console.log);
                     }
-                }
+                },
+                complete: () => this.data.count = this.data.time = 0
             });
         }
     }
